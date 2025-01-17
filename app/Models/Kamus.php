9b72f4f1faba3c50cd16dfaa32d0a78e469d6a15
Ergavibe <arsyverenada@gmail.com>
2025-01-18 02:05:55 +0700

    Fixing Dictionary System

--- app/Models/Kamus.php
+++ app/Models/Kamus.php
@@ -14,6 +14,11 @@ class Kamus extends Model
         'id',
     ];
 
+    protected $fillable = [
+        'link',
+        'categories_id'
+    ]; 
+
     public function category(){
         return $this->belongsTo(Category::class, 'categories_id');
     }
